7a7fff9cca57df06c2942a3aa912cc1e39776924
Cat Flynn <me@ktyl.dev>
2024-10-25 23:46:39 +0100

    make ~ easier to reach

--- keyboards/ergodox_ez/keymaps/ktyl/keymap.c
+++ keyboards/ergodox_ez/keymaps/ktyl/keymap.c
@@ -24,8 +24,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |----------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift   |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `----------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Ctrl | '"  | Grv/L1| Alt  | LGui |                                       | AltGr | Alt |   [  |   ]  | ~L1  |
- *   `----------------------------------'                                       `----------------------------------'
+ *   | Ctrl | Grv |  ~ | Alt  | LGui |                                       | AltGr | Alt |   [  |   ]  | ~L1  |
+ *   `--------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-----------.
  *                                        | App  | Right|       | Down | Up |
  *                                 ,------|------|------|       |------+------+-------.
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    TG(SYMB),             TG(SYMB),     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS,
   CTL_T(KC_ESC),   KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), GUI_T(KC_QUOT),
   KC_LSFT,         KC_Z,        KC_X,          KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),         MEH_T(KC_NO), KC_N,    KC_M,    KC_COMM, KC_DOT,           CTL_T(KC_SLSH), KC_RSFT,
-  KC_LCTL, KC_QUOT,     LT(SYMB,KC_GRV),       KC_LALT, KC_LGUI,                                              KC_ALGR,   KC_LALT, KC_LBRC, KC_RBRC, TT(SYMB),
+  KC_LCTL,          KC_GRV,            KC_TILD,        KC_LALT, KC_LGUI,                                              KC_ALGR,   KC_LALT, KC_LBRC, KC_RBRC, TT(SYMB),
                                                                  KC_LEFT, KC_RGHT,                 KC_DOWN, KC_UP,
                                                                           KC_HOME,                 KC_PGUP,
                                                          KC_SPC, KC_BSPC, KC_END,                  KC_PGDN, KC_DEL, KC_ENT
